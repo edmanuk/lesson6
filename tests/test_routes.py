6c958e153b5b16e31b699950ab9f060d25fa66e2
Edgar <edgarik25@mail.ru>
2020-11-14 18:50:40 +0300

    update route and test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,7 +3,7 @@ import pytest
 def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
-    assert res.data == b"Base action"
+    assert res.data == b"Hello from CI with GitHub Actions by Manukyan"
 
 
 def test_v2(app, client):
