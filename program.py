d452ac3e4072f307740e1ff1952e31105d7f0dab
Edgar <edgarik25@mail.ru>
2020-11-14 19:07:52 +0300

    update route and test

--- program.py
+++ program.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from CI with GitHub Actions by Manukyan'
+    return 'Base action'
 
 @app.route('/v2')
 def v2():
     return 'Second action'
+
+@app.route('/manukyan')
+def manukyan():
+    return 'Hello from CI with GitHub Actions by Manukyan'
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,10 +3,16 @@ import pytest
 def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
-    assert res.data == b"Hello from CI with GitHub Actions by Manukyan"
+    assert res.data == b"Base action"
 
 
 def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+
+def test_manukyan(app, client):
+    res = client.get('/manukyan')
+    assert res.status_code == 200
+    assert res.data == b"Hello from CI with GitHub Actions by Manukyan"
